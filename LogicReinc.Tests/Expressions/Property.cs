91ef524c2013c2e1660c4ff33c24142acef3c9d2
Kelvin K <logicincarnate@gmail.com>
2017-01-04 11:04:44 +0100

    Add Expression builders and tests

---
+++ LogicReinc.Tests/Expressions/Property.cs
@@ -0,0 +1,113 @@
+﻿using LogicReinc.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicReinc.Tests.Expressions
+{
+    [TestClass]
+    public class PropertyTests
+    {
+        const string _testValue = "Abc";
+        const int _testItterations = 1000000;
+
+
+        static TestClass _testObj;
+        static Func<object, object> _propGetter;
+        static Action<object, object> _propSetter;
+
+        //Reflection for speed comparison
+        static PropertyInfo _propInfo = typeof(TestClass).GetProperty("SomeName");
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            _testObj = new TestClass()
+            {
+                SomeName = _testValue
+            };
+            _propGetter = Property.BuildPropertyGetter("SomeName", typeof(TestClass));
+            _propSetter = Property.BuildPropertySetter("SomeName", typeof(TestClass));
+        }
+
+        #region PropetyGetter
+        [TestMethod]
+        public void BuildPropertyGetter()
+        {
+            var getter = Property.BuildPropertyGetter("SomeName", typeof(TestClass));
+        }
+
+        [TestMethod]    //1,000,000* = 15-20ms
+        public void GetProperty()
+        {
+            for (int i = 0; i < _testItterations; i++)
+            {
+                object val = _propGetter(_testObj);
+            }
+        }
+
+        [TestMethod]    //1,000,000* = 4-7ms
+        public void GetPropertyDirect()
+        {
+            for (int i = 0; i < _testItterations; i++)
+            {
+                object val = _testObj.SomeName;
+            }
+        }
+
+        [TestMethod]    //1,000,000* = 200ms
+        public void GetPropertyReflection()
+        {
+            for (int i = 0; i < _testItterations; i++)
+            {
+                object val = _propInfo.GetValue(_testObj);
+            }
+        }
+        #endregion
+
+
+        #region PropertySetter
+        [TestMethod]
+        public void BuildPropertySetter()
+        {
+            var setter = Property.BuildPropertySetter("SomeName", typeof(TestClass));
+        }
+
+        [TestMethod]    //1,000,000* = 17-20ms
+        public void SetProperty()
+        {
+            for (int i = 0; i < _testItterations; i++)
+            {
+                _propSetter(_testObj, _testValue);
+            }
+        }
+
+        [TestMethod]    //1,000,000* = 5-7ms
+        public void SetPropertyDirect()
+        {
+            for (int i = 0; i < _testItterations; i++)
+            {
+                _testObj.SomeName = _testValue;
+            }
+        }
+
+        [TestMethod]    //1,000,000* = 300ms
+        public void SetPropertyReflection()
+        {
+            for (int i = 0; i < _testItterations; i++)
+            {
+                _propInfo.SetValue(_testObj, _testValue);
+            }
+        }
+        #endregion
+
+        public class TestClass
+        {
+            public string SomeName { get; set; }
+        }
+    }
+}
---
+++ LogicReinc/Expressions/Property.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicReinc.Expressions
+{
+    public class Property
+    {
+
+        public static Func<object, object> BuildPropertyGetter(string property, Type type)
+        {
+            PropertyInfo prop = type.GetProperty(property);
+            if (prop == null)
+                throw new ArgumentException($"Property [{property}] does not exist");
+
+            //Parameters
+            ParameterExpression arg = Expression.Parameter(typeof(object), "obj");
+
+            //Conversion
+            UnaryExpression convertedArg = Expression.Convert(arg, type);
+
+            //Property
+            Expression getExpression = Expression.Property(convertedArg, property);
+            
+
+            //(obj) => obj.[Property];
+            return Expression.Lambda<Func<object, object>>(getExpression, arg).Compile();
+        }
+
+
+        public static Action<object, object> BuildPropertySetter(string property, Type type)
+        {
+            PropertyInfo prop = type.GetProperty(property);
+            if (prop == null)
+                throw new ArgumentException($"Property [{property}] does not exist");
+
+            MethodInfo setMethod = prop.GetSetMethod();
+            if (setMethod == null)
+                throw new ArgumentException($"Property [{property}] has no Set Method");
+
+            //Parameter
+            //(object obj, object val)
+            ParameterExpression argObj = Expression.Parameter(typeof(object), "obj");
+            ParameterExpression argVal = Expression.Parameter(typeof(object), "val");
+
+            //Casting
+            //([InstanceType])[obj];
+            UnaryExpression argObjCasted = Expression.Convert(argObj, type);
+            //([PropertyType])[val];
+            UnaryExpression argValCasted = Expression.Convert(argVal, prop.PropertyType);
+
+            //Call
+            //obj.[Property] = ([PropertyType])[val];
+            MethodCallExpression call = Expression.Call(argObjCasted, setMethod, argValCasted);
+
+            //(obj, val) => obj[Property] = [val]
+            return Expression.Lambda<Action<object, object>>(call, argObj, argVal).Compile();
+        }
+    }
+}

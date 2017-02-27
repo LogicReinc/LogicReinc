a75610db835de311bdc73106bee00c1875f6c395
Kelvin K <logicincarnate@gmail.com>
2017-02-27 16:54:59 +0100

    Change TokenSystem and adding some reflection

--- LogicReinc/Extensions/TypeExtensions.cs
+++ LogicReinc/Extensions/TypeExtensions.cs
@@ -11,6 +11,7 @@ namespace LogicReinc.Extensions
     public static class TypeExtensions
     {
         private static Dictionary<Type, PropertyInfo[]> CachedProperties { get; } = new Dictionary<Type, PropertyInfo[]>();
+        private static Dictionary<Type, MethodInfo[]> CachedMethods { get; } = new Dictionary<Type, MethodInfo[]>();
 
         public static PropertyInfo[] GetPropertiesCached(this Type type)
         {
@@ -19,6 +20,19 @@ namespace LogicReinc.Extensions
             return CachedProperties[type];
         }
 
+        public static MethodInfo[] GetMethodsCached(this Type type)
+        {
+            if (!CachedMethods.ContainsKey(type))
+                CachedMethods.Add(type, type.GetMethods());
+            return CachedMethods[type];
+        }
+
+        public static MethodInfo GetMethodCached(this Type type, string name)
+        {
+            if (!CachedMethods.ContainsKey(type))
+                CachedMethods.Add(type, type.GetMethods());
+            return CachedMethods[type].FirstOrDefault(x => x.Name == name);
+        }
 
         public static bool IsObject(this Type type)
         {

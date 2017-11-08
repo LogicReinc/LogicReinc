f403be37dd09d8b4d1009b0e3e567fc8981da5fc
LogicReinc <LogicReinc@users.noreply.github.com>
2017-11-08 17:19:44 +0100

    WIP TcpHttpClient and template for handlers

---
+++ LogicReinc/Templates/HandlerBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicReinc.Templates
+{
+    public class HandlerBase<HandlerType, IdentifierType, ContextObject> 
+        where HandlerType : HandlerBase<HandlerType, IdentifierType, ContextObject>
+    {
+        static Dictionary<IdentifierType, MethodInfo> _handlers = new Dictionary<IdentifierType, MethodInfo>();
+
+        static HandlerBase()
+        {
+            MethodInfo[] infos = typeof(HandlerType).GetMethods(BindingFlags.Static | BindingFlags.Public).Where(x => Attribute.IsDefined(x, typeof(HandlerAttribute))).ToArray();
+
+            _handlers = infos.ToDictionary(x => (IdentifierType)((HandlerAttribute)Attribute.GetCustomAttribute(x, typeof(HandlerAttribute))).Value, y => y);
+        }
+
+        public static void Handle(IdentifierType key, ContextObject obj)
+        {
+            if (_handlers.ContainsKey(key))
+                _handlers[key].Invoke(null, new object[] { obj });
+            else
+                throw new Exception("Key does not exist");
+        }
+    }
+
+    public class HandlerAttribute : Attribute
+    {
+        public object Value { get; set; }
+
+        public HandlerAttribute(object obj)
+        {
+            Value = obj;
+        }
+    }
+}
